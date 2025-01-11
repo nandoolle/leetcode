56e7190ff75927ba4ce3649a2c68c6dbf1a7e3da
Fernando Ollé <fernando.olle@icloud.com>
2025-01-10 23:11:27 -0300

    add commetaries to generate parentheses

--- neetcode/stacks/generate_parentheses.js
+++ neetcode/stacks/generate_parentheses.js
@@ -5,13 +5,24 @@ function generateParenthesis(n) {
   let closeCount = 0;
 
   function backtrack(openCount, closeCount) {
+    // by cheking the counts dont pass N we garantee that the number
+    // of valid pairs dont passes the N 
     if (openCount === n && closeCount === n) {
+      // here we can assume that we reached one the leaves fo our decision
+      // so we add the result to the result list 
       result.add(curr.join(''));
+      // by only returning here we garantee that all possible 
+      // combinations have been discovered
       return;
     }
+    // those 2 conditions bellow garantee that all possible
+    // combinations will be build, no matter the order
     if (openCount < n) {
       curr.push('(');
       backtrack(openCount + 1, closeCount);
+      // here we pop the last char inserted onto our global variable;
+      // we only need to pop one at a time and the recursive call 
+      // unwinding makes sure make sure the global var was emptied  
       curr.pop();
     }
     if (openCount > closeCount) {
