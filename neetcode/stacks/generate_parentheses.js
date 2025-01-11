7654ffffb2db5d778654396866f9525f468ab118
Fernando Ollé <fernando.olle@icloud.com>
2025-01-10 23:28:13 -0300

    add simpler generate parentheses

--- neetcode/stacks/generate_parentheses.js
+++ neetcode/stacks/generate_parentheses.js
@@ -1,38 +1,23 @@
 function generateParenthesis(n) {
-  const result = new Set();
-  const curr = [];
+  const result = [];
   let openCount = 0;
   let closeCount = 0;
 
-  function backtrack(openCount, closeCount) {
-    // by cheking the counts dont pass N we garantee that the number
-    // of valid pairs dont passes the N 
-    if (openCount === n && closeCount === n) {
-      // here we can assume that we reached one the leaves fo our decision
-      // so we add the result to the result list 
-      result.add(curr.join(''));
-      // by only returning here we garantee that all possible 
-      // combinations have been discovered
+  function backtrack(openN, closedN, n, result, stack) {
+    if (openN === closedN && openN === n) {
+      result.push(stack);
       return;
     }
-    // those 2 conditions bellow garantee that all possible
-    // combinations will be build, no matter the order
-    if (openCount < n) {
-      curr.push('(');
-      backtrack(openCount + 1, closeCount);
-      // here we pop the last char inserted onto our global variable;
-      // we only need to pop one at a time and the recursive call 
-      // unwinding makes sure make sure the global var was emptied  
-      curr.pop();
+
+    if (openN < n) {
+      backtrack(openN + 1, closedN, n, result, stack + '(');
     }
-    if (openCount > closeCount) {
-      curr.push(')');
-      backtrack(openCount, closeCount + 1);
-      curr.pop();
+    if (closedN < openN) {
+      backtrack(openN, closedN + 1, n, result, stack + ')');
     }
   }
 
-  backtrack(openCount, closeCount);
+  backtrack(openCount, closeCount, n, result, '');
   return result;
 }
 
