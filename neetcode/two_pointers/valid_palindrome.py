eac00cc7b4bee2411c320471ee064200613d7326
Fernando Ollé <fernando.olle@icloud.com>
2025-01-13 23:35:43 -0300

    simplify is_palindrome

--- neetcode/two_pointers/valid_palindrome.py
+++ neetcode/two_pointers/valid_palindrome.py
@@ -2,23 +2,14 @@ import re
 
 def isPalindrome(s: str) -> bool:
     parsed_string = re.sub(r'[^0-9A-Z]', '', s.upper())
-    LEN = len(parsed_string)
-    left, right = LEN//2 -1, LEN//2
-    
-    if LEN == 0 or LEN == 1:
-        return True
+    left, right = 0, len(parsed_string) - 1
 
-    if LEN % 2 != 0:
-        left = LEN//2 
-        right = LEN//2
-
-    while right < LEN:
+    while right >= left:
         if parsed_string[left] != parsed_string[right]:
-            print(parsed_string[left], parsed_string[right], left, right)
-
             return False
-        left = left -1
-        right = right +1
+
+        left += 1
+        right -= 1
 
     return True
 
